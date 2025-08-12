b3993de23f27761cb85df52faac5735e2f726ba4
JPivista <jpivistasolutions@gmail.com>
2025-08-12 16:04:00 +0530

    menu item is updatd

--- src/components/Mca/McaContent.js
+++ src/components/Mca/McaContent.js
@@ -7,13 +7,13 @@ const McaContent = () => {
             <div className='container mx-auto flex flex-col items-center justify-center'>
                 <div className='text-center lg:w-[80%]  py-10'>
                     <p>
-                        With a legacy of over 30 years, the B.Com program at AIMS bridges academic theory with industry demands. Designed for ambitious undergraduates, it combines core learning with certifications and real-world case exposure.
+                        AIMS MCA is structured to develop skilled professionals with strong technical foundations, software development expertise, and industry-readiness. Designed to keep pace with emerging technologies, the program is tailored for students looking to build impactful careers in IT, analytics, and beyond.
                     </p>
                 </div>
 
                 <div className='text-center'>
                     <h3 className='text-[#531675]'>
-                        Why Choose <span className='italic playfair-400'>B.Com at AIMS ?</span>
+                        Why Choose <span className='italic playfair-400'>MCA at AIMS ?</span>
                     </h3>
                 </div>
 
