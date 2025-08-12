02add4c48ba322e280a4085c8ea3cbbab570a353
sibiraj-002 <shansibi2002@gmail.com>
2025-08-12 15:56:05 +0530

    Centre for research and Library and information centre page completed

--- src/components/CentreForResearchComponents/ChooseYourGoal.js
+++ src/components/CentreForResearchComponents/ChooseYourGoal.js
@@ -4,8 +4,8 @@ const ChooseYourGoal = () => {
     return (
         <div className='container mx-auto py-15 px-4'>
             <h3 className='text-center text-[#0C2165] font-semibold'>Choose Your Goal. AIM for the Future.</h3>
-            <h6 className='text-center'>At AIMS, we believe research is more than exploration; it’s a way to focus your academic potential and move purposefully towards your professional and social impact goals. Established in 2005, the AIMS Centre for Research (ACR) was founded to nurture a structured, goal-oriented research culture across the institution.</h6>
-            <h6 className='text-center pt-5'>Whether you're a student aiming to hone your research skills or a faculty member looking to deepen academic contributions, ACR offers guidance, support, and strategic direction, making AIMS one of the best colleges in Bangalore for research projects.</h6>
+            <h6 className='text-center'>At AIMS, we believe research is more than exploration; it&apos; s a way to focus your academic potential and move purposefully towards your professional and social impact goals. Established in 2005, the AIMS Centre for Research (ACR) was founded to nurture a structured, goal-oriented research culture across the institution.</h6>
+            <h6 className='text-center pt-5'>Whether you&apos;re a student aiming to hone your research skills or a faculty member looking to deepen academic contributions, ACR offers guidance, support, and strategic direction, making AIMS one of the best colleges in Bangalore for research projects.</h6>
         </div>
     )
 }
