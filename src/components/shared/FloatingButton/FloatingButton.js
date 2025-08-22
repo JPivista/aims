06f76b617ce62c075c1472bf383a8421099b9a15
JPivista <jpivistasolutions@gmail.com>
2025-08-22 16:48:38 +0530

    floating button is updated

--- src/components/shared/FloatingButton/FloatingButton.js
+++ src/components/shared/FloatingButton/FloatingButton.js
@@ -5,30 +5,33 @@ import { GoArrowDownRight } from "react-icons/go";
 
 const FloatingButton = () => {
     return (
-        <div className="fixed right-3 top-1/3 flex flex-col md:gap-32 gap-26 z-[9999]">
+        <div className="fixed right-3 top-[35%] flex flex-col z-[9999] -rotate-90 transform origin-right">
             {/* Enquire Now Button */}
-            <Link
-                href="https://enquiry.theaims.ac.in/"
-                target="_blank"
-                className="bg-[#9c2474] text-white font-semibold text-xs md:text-sm px-3 md:px-4 py-2 rounded-tl-lg rounded-tr-lg shadow-lg transform -rotate-90 origin-right hover:bg-[#FF7F02] transition-all duration-300 group flex items-center justify-center"
-            >
-                <span className="flex items-center justify-center gap-1 md:gap-2">
-                    Enquire Now
-                    <GoArrowDownRight className="w-3 h-3 md:w-4 md:h-4 transition-all duration-300 ease-in-out group-hover:-rotate-90" />
-                </span>
-            </Link>
+            <div className="flex gap-4">
+                {/* Apply Now Button */}
+                <Link
+                    href="https://apply.theaims.ac.in/login"
+                    target="_blank"
+                    className="bg-[#9c2474] text-white font-semibold text-xs md:text-sm px-3 md:px-4 py-1 rounded-tl-lg rounded-tr-lg shadow-lg transform origin-right hover:bg-[#FF7F02] transition-all duration-300 group flex items-center justify-center"
+                >
+                    <span className="flex items-center justify-center gap-1 md:gap-2">
+                        Apply Now
+                        <GoArrowDownRight className="w-3 h-3 md:w-4 md:h-4 transition-all duration-300 ease-in-out group-hover:-rotate-90" />
+                    </span>
+                </Link>
 
-            {/* Apply Now Button */}
-            <Link
-                href="https://apply.theaims.ac.in/login"
-                target="_blank"
-                className="bg-[#9c2474] text-white font-semibold text-xs md:text-sm px-3 md:px-4 py-2 rounded-tl-lg rounded-tr-lg shadow-lg transform -rotate-90 origin-right hover:bg-[#FF7F02] transition-all duration-300 group flex items-center justify-center"
-            >
-                <span className="flex items-center justify-center gap-1 md:gap-2">
-                    Apply Now
-                    <GoArrowDownRight className="w-3 h-3 md:w-4 md:h-4 transition-all duration-300 ease-in-out group-hover:-rotate-90" />
-                </span>
-            </Link>
+                {/* Enquire Now Button */}
+                <Link
+                    href="https://enquiry.theaims.ac.in/"
+                    target="_blank"
+                    className="bg-[#9c2474] text-white font-semibold text-xs md:text-sm px-3 md:px-4 py-1 rounded-tl-lg rounded-tr-lg shadow-lg transform  origin-right hover:bg-[#FF7F02] transition-all duration-300 group flex items-center justify-center"
+                >
+                    <span className="flex items-center justify-center gap-1 md:gap-2">
+                        Enquire Now
+                        <GoArrowDownRight className="w-3 h-3 md:w-4 md:h-4 transition-all duration-300 ease-in-out group-hover:-rotate-90" />
+                    </span>
+                </Link>
+            </div>
         </div>
     );
 };
