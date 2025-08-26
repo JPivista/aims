2b994bb3f7c6dcea97b7502e1ff639c9e4845cd6
sibiraj-002 <shansibi2002@gmail.com>
2025-08-26 23:21:20 +0530

    Updated all page images from PNG to WebP for optimization, and refreshed PDF files in Previous Years Question Paper page

--- src/components/CommerceCeba/Banner.js
+++ src/components/CommerceCeba/Banner.js
@@ -20,6 +20,8 @@ const ScienceIntegratedPcmbPcmcCetNeetJeeBanner = ({ announcements, pageType = '
                     }}
                 ></div>
 
+
+
                 {/* Text/Logo Content */}
                 <div className="absolute inset-0 flex flex-col justify-between z-10">
                     {/* Logo Block */}
