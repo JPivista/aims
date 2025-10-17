76e13d14f99c51b09148ce6792953ae147758387
JPivista <jpivistasolutions@gmail.com>
2025-10-17 15:23:36 +0530

    mca is image is updated

--- src/components/SchoolOfInformationTechnology/SoInfoTechPrograms.js
+++ src/components/SchoolOfInformationTechnology/SoInfoTechPrograms.js
@@ -24,7 +24,7 @@ const SoInfoTechPrograms = () => {
         id: 'doctoral',
         title: 'Master of Computer Applications (MCA)',
         description: 'Our MCA program transforms learners into capable IT professionals with a sharp eye for innovation. The program is accredited by AICTE and designed to meet global IT standards.',
-        image: '/school-of-info-tech/mca-1.webp', // You'll need to add this image
+        image: '/school-of-info-tech/mca.webp', // You'll need to add this image
         link: '/information-technology-school/master-computer-applications'
     }
 
